a64e94055d97a866c3b74915581b18f98826233d
Caleb Hollander <caleb.hollander@coforge.com>
2026-08-25 15:12:01 +0000

    Caleb's status tracker app

--- StatusTracker/app/main.py
+++ StatusTracker/app/main.py
@@ -1,5 +1,8 @@
+from re import S
 from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy.orm import Session
+
+from sqlalchemy import select
 
 from . import models, schemas
 from .database import Base, engine, get_db
@@ -14,34 +17,73 @@ def root():
     return {"message": "Broken Task API"}
 
 
+# Changes not being committed. TODO: Commit changes
+# Actually have to update database
+# Completed should = false on initial creation
 @app.post("/tasks", response_model=schemas.TaskResponse, status_code=status.HTTP_201_CREATED)
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    print("CREATING TASK")
     new_task = models.Task(
         title=task.title,
         description=task.description,
+        # starting off with completion = False but is default behavior already
+        #completed = False
     )
+
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
     return new_task
 
-
+# 
 @app.get("/tasks/{task_id}", response_model=schemas.TaskResponse)
 def get_task(task_id: int, db: Session = Depends(get_db)):
-    raise HTTPException(
-        status_code=status.HTTP_501_NOT_IMPLEMENTED,
-        detail="Read is not implemented",
+
+    statement = select(models.Task).where(models.Task.id == task_id)
+    task = db.execute(statement).scalar()
+
+    if task is None:
+        raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Task not found",
     )
+    return task
 
-
+#TODO: Implement
+# should be able to update title and description + completion status
 @app.put("/tasks/{task_id}", response_model=schemas.TaskResponse)
 def update_task(task_id: int, task: schemas.TaskUpdate, db: Session = Depends(get_db)):
-    raise HTTPException(
-        status_code=status.HTTP_501_NOT_IMPLEMENTED,
-        detail="Update is not implemented",
-    )
+
+    statement = select(models.Task).where(models.Task.id == task_id)
+    retrievedTask = db.execute(statement).scalar()
+
+    if retrievedTask is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Task not found",
+        )
+
+    if task.title:
+        retrievedTask.title = task.title
+
+    if task.description:
+        retrievedTask.description = task.description
+    
+    if task.completed:
+        retrievedTask.completed = task.completed
+
+
+
+    db.commit()
+    return retrievedTask
 
 
 @app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_task(task_id: int, db: Session = Depends(get_db)):
-    task = db.query(models.Task).order_by(models.Task.id).first()
+    # Only deleting first row of database. TODO: FIX THIS
+    #task = db.query(models.Task).order_by(models.Task.id).first()
+    statement = select(models.Task).where(models.Task.id == task_id)
+    task = db.execute(statement).scalar()
 
     if task is None:
         raise HTTPException(
--- StatusTracker/tests/test_app.py
+++ StatusTracker/tests/test_app.py
@@ -1,9 +1,27 @@
+#TODO: Create Unit Tests + unit test coverage
+
 from fastapi.testclient import TestClient
 
 from app.main import app
 
 client = TestClient(app)
 
+import pytest
+
+"""
+Sample data to use for most task operations
+"""
+@pytest.fixture
+def sample_task_with_title_and_description():
+    return {"title": "TEST TITLE", "description": "TEST DESCRIPTION"}
+
+
+"""
+Task with id, title, description, and completed fields
+"""
+@pytest.fixture
+def sample_all_fields_task():
+    return {"id": 67, "title": "67 FULL TEST TITLE", "description": "FULL TEST DESCRIPTION", "completed": False}
 
 def test_root_returns_something():
     response = client.get("/")
@@ -12,6 +30,29 @@ def test_root_returns_something():
 
 def test_python_still_does_math():
     assert 10 + 5 == 15
+
+def test_get_root():
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_create_task_with_no_title():
+    response = client.post("/tasks", json = {"description": "description"})
+    assert response.status_code == 422
+
+def test_create_task_with_valid_title_and_description(sample_task_with_title_and_description):
+    payload = {"title": sample_task_with_title_and_description["title"], "description": sample_task_with_title_and_description["description"]}
+    response = client.post("/tasks", json = payload)
+    assert response.status_code == 201
+
+#TODO: Set up test DB connection, add sample data (sample_all_fields_task), and test GET using test fixtures making sure that the returned task matches sample_all_fields_task
+#Started Implementation Below
+# def test_get_task_by_valid_task_id():
+#     response = client.get("/tasks", params = {"task_id": })
+
+#TODO: Create tests for all other endpoints
+#TODO: Create tests for database connection
+
+
 
 """
 Running the Application
